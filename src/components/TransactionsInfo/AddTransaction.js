ac4e7f31bb549df57c3d9f963f700d3f9e5cc18b
MuhammadNaeemAkhtar <mnakhtar247@gmail.com>
2020-06-12 16:12:29 +0200

    Floating point addintion

--- src/components/TransactionsInfo/AddTransaction.js
+++ src/components/TransactionsInfo/AddTransaction.js
@@ -30,11 +30,11 @@ export const AddTransaction = () => {
                 <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
                 </div>
                 <div className="form-control">
-                <label htmlfor="amount"
-                    >Amount <br />
-                    (negative - expense, positive - income)</label
-                >
-                <input type="number" onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+                <label htmlfor="amount">
+                    Amount <br />
+                    (negative - expense, positive - income)
+                </label>
+                <input type="number" step="any" onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
